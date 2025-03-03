3951671e3627208e7f664b27168c366a27fdbf51
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-03 22:02:29 +0100

    Adding facade level create_review method

--- part2/hbnb_layered_facades/app/services/facades/review_facade.py
+++ part2/hbnb_layered_facades/app/services/facades/review_facade.py
@@ -1,6 +1,14 @@
 from app.persistence.gateways.review_gateway import ReviewGateway
 from app.models.review_model import Review
+from app.services.exception import InvalidReviewData, ReviewNotFound
 
 class ReviewFacade:
     def __init__(self):
         self.gateway = ReviewGateway()
+        
+    def create_review(self, data):
+        review = Review(**data)
+        verif = review.format_validation()
+        if not verif:
+            raise InvalidReviewData
+        return self.gateway.add(review)
