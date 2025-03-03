ce3012ec93c98178a2ffb9359606968e4f496002
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-03 22:00:57 +0100

    Adding init and add to Review Gateway

--- part2/hbnb_layered_facades/app/persistence/gateways/review_gateway.py
+++ part2/hbnb_layered_facades/app/persistence/gateways/review_gateway.py
@@ -2,4 +2,9 @@ from app.persistence.gateways.repository import InMemoryRepository
 from app.models.user_model import User
 
 class ReviewGateway(InMemoryRepository):
-    pass
+    def __init__(self):
+        super().__init__()
+        
+    def add(self, obj):
+        self._storage[obj.id] = obj
+        return obj
