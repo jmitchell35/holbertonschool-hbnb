e0db5f0872d9bf4a8387d17aff9c388c94d48971
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-03 13:00:47 +0100

    Updating place endpoints with updated models and marshaling

--- part2/hbnb_layered_facades/app/api/v1/place_endpoints.py
+++ part2/hbnb_layered_facades/app/api/v1/place_endpoints.py
@@ -5,27 +5,7 @@ from app.services.exception import (InvalidPlaceData, OwnerNotFound,
 
 api = Namespace('places', description='Place operations')
 
-review_model = api.model('PlaceReview', {
-    'id': fields.String(description='Review ID'),
-    'text': fields.String(description='Text of the review'),
-    'rating': fields.Integer(description='Rating of the place (1-5)'),
-    'user_id': fields.String(description='ID of the user')
-})
-
-amenity_model = api.model('Amenity', {
-    'name': fields.String(required=True, description='Name of the amenity')
-})
-
-user_model = api.model('User', {
-    'first_name': fields.String(
-        required=True, description='First name of the user'),
-    'last_name': fields.String(
-        required=True, description='Last name of the user'),
-    'email': fields.String(
-        required=True, description='Email of the user')
-})
-
-place_model = api.model('Place', {
+place_input_model = api.model('Place', {
     'title': fields.String(
         required=True, description='Title of the place'),
     'description': fields.String(
@@ -40,10 +20,52 @@ place_model = api.model('Place', {
         required=True, description='ID of the owner'),
 })
 
+review_output_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
+
+amenity_output_model = api.model('Amenity', {
+    'id': fields.String(description='Amenity ID'),
+    'name': fields.String(required=True, description='Name of the amenity')
+})
+
+user_output_model = api.model('User', {
+    'id': fields.String(description='User ID'),
+    'first_name': fields.String(
+        required=True, description='First name of the user'),
+    'last_name': fields.String(
+        required=True, description='Last name of the user'),
+    'email': fields.String(
+        required=True, description='Email of the user')
+})
+
+place_output_model = api.model('Place', {
+    'id': fields.String(description='Place ID'),
+    'title': fields.String(
+        required=True, description='Title of the place'),
+    'description': fields.String(
+        description='Description of the place'),
+    'price': fields.Float(
+        required=True, description='Price per night'),
+    'latitude': fields.Float(
+        required=True, description='Latitude of the place'),
+    'longitude': fields.Float(
+        required=True, description='Longitude of the place'),
+    'owner_id': fields.String(
+        required=True, description='ID of the owner'),
+    'owner': fields.Nested(user_output_model, description='Owner of the place'),
+    'amenities': fields.List(fields.Nested(amenity_output_model), description='List of amenities'),
+    'reviews': fields.List(fields.Nested(review_output_model), description='List of reviews')
+})
+
 @api.route('/')
 class PlaceList(Resource):
 
-    @api.expect(place_model, validate=True)
+    @api.expect(place_input_model, validate=True)
+    @api.marshal_with(place_input_model, code=201)
     @api.response(201, 'Place successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
@@ -52,30 +74,22 @@ class PlaceList(Resource):
 
         try:
             place = facade.place_manager.create_place(place_data)
-            return {
-                'id': place.id,
-                'title': place.title,
-                'description': place.description,
-                'price': place.price,
-                'latitude': place.latitude,
-                'longitude': place.longitude,
-                'owner_id': place.owner_id,
-            }, 201
+            return place, 201
         except InvalidPlaceData:
             return {'error': 'Invalid input data'}, 400
         except OwnerNotFound:
             return {'error': 'Owner not found'}, 404
 
     @api.response(200, 'List of places retrieved successfully')
+    @api.marshal_list_with(place_output_model)
     def get(self):
         """Retrieve a list of all places"""
-        return facade.place_facade.get_all_places()
+        list_of_places = facade.place_facade.get_all_places()
+        return list_of_places
 
 @api.route('/<place_id>')
-
 @api.doc(params={'place_id': 'The place ID'})
 class PlaceResource(Resource):
-    @api.expect(place_model, validate=False)
     @api.response(200, 'Place details retrieved successfully')
     @api.response(404, 'Place not found')
     def get(self, place_id):
@@ -98,7 +112,7 @@ class PlaceResource(Resource):
         except AmenityNotFound:
             return {'error': 'Amenity not found'}, 404
     
-    @api.expect(place_model)
+    @api.expect(place_input_model)
     @api.response(200, 'Place updated successfully')
     @api.response(404, 'Place not found')
     @api.response(400, 'Invalid input data')
