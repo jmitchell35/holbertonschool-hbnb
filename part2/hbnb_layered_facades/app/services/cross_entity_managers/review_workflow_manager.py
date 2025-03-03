150c09d9d71792cd67276065bfa7b5c7ab57e252
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-03 22:02:08 +0100

    Adding create_review method

--- part2/hbnb_layered_facades/app/services/cross_entity_managers/review_workflow_manager.py
+++ part2/hbnb_layered_facades/app/services/cross_entity_managers/review_workflow_manager.py
@@ -1,6 +1,5 @@
-from app.services.exception import (UserNotFound, InvalidPlaceData,
-                                    OwnerNotFound, PlaceNotFound,
-                                    AmenityNotFound)
+from app.services.exception import (UserNotFound, PlaceNotFound,
+                                    InvalidReviewData)
 
 class ReviewWorkflowManager:
     def __init__(self, place_facade, user_facade, review_facade):
@@ -8,4 +7,12 @@ class ReviewWorkflowManager:
         self.user_facade = user_facade
         self.review_facade = review_facade
     
-    
+    def create_review(self, data):
+        try:
+            self.user_facade.get(data['user_id'])
+            self.place_facade.get(data['place_id'])
+            return self.review_facade.create_review(data)
+        except UserNotFound:
+            raise UserNotFound
+        except PlaceNotFound:
+            raise InvalidReviewData
