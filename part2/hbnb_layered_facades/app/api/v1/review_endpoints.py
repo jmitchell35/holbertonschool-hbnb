9846504577ade98712ff38d370b069e6f28a7fdd
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-04 09:14:35 +0100

    Setting up review endpoints methods, adding data models for inputs and outputs

--- part2/hbnb_layered_facades/app/api/v1/review_endpoints.py
+++ part2/hbnb_layered_facades/app/api/v1/review_endpoints.py
@@ -1,48 +1,65 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from app.services.exception import (InvalidReviewData, ReviewNotFound,
+                                    PlaceNotFound)
 
 api = Namespace('reviews', description='Review operations')
 
 # Define the review model for input validation and documentation
-review_model = api.model('Review', {
+review_input_model = api.model('ReviewInput', {
     'text': fields.String(required=True, description='Text of the review'),
-    'rating': fields.Integer(required=True, description='Rating of the place (1-5)'),
+    'rating': fields.Integer(required=True,
+                             description='Rating of the place (1-5)'),
+    'user_id': fields.String(required=True, description='ID of the user'),
+    'place_id': fields.String(required=True, description='ID of the place')
+})
+review_output_model = api.model('ReviewOutput', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(required=True, description='Text of the review'),
+    'rating': fields.Integer(required=True,
+                             description='Rating of the place (1-5)')
+})
+review_details_model = api.model('ReviewDetails', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(required=True, description='Text of the review'),
+    'rating': fields.Integer(required=True,
+                             description='Rating of the place (1-5)'),
     'user_id': fields.String(required=True, description='ID of the user'),
     'place_id': fields.String(required=True, description='ID of the place')
 })
 
 @api.route('/')
 class ReviewList(Resource):
-    @api.expect(review_model, validate=True)
+    @api.expect(review_input_model, validate=True)
     @api.response(201, 'Review successfully created')
     @api.response(400, 'Invalid input data')
+    @api.marshal_with(review_details_model, code=201)
     def post(self):
         """Register a new review"""
         review = facade.review_manager.create_review(api.payload)
-        return {
-            'id': review.id,
-            'text': review.text,
-            'rating': review.rating,
-            'user_id': review.user_id,
-            'place_id': review.place_id
-            }, 201
+        return review, 201
 
     @api.response(200, 'List of reviews retrieved successfully')
+    @api.marshal_list_with(review_output_model, code=200)
     def get(self):
         """Retrieve a list of all reviews"""
-        # Placeholder for logic to return a list of all reviews
-        pass
+        return facade.review_facade.get_all_reviews()
 
 @api.route('/<review_id>')
+@api.doc(params={'review_id': 'The review ID'})
 class ReviewResource(Resource):
     @api.response(200, 'Review details retrieved successfully')
     @api.response(404, 'Review not found')
+    @api.marshal_with(review_details_model, code=200)
     def get(self, review_id):
         """Get review details by ID"""
-        # Placeholder for the logic to retrieve a review by ID
-        pass
+        try:
+            review = facade.review_facade.get(review_id)
+            return review, 200
+        except ReviewNotFound:
+            return {'error': 'Review not found'}, 404
 
-    @api.expect(review_model)
+    @api.expect(review_input_model)
     @api.response(200, 'Review updated successfully')
     @api.response(404, 'Review not found')
     @api.response(400, 'Invalid input data')
@@ -59,10 +76,15 @@ class ReviewResource(Resource):
         pass
 
 @api.route('/places/<place_id>/reviews')
+@api.doc(params={'place_id': 'The place ID'})
 class PlaceReviewList(Resource):
     @api.response(200, 'List of reviews for the place retrieved successfully')
     @api.response(404, 'Place not found')
+    @api.marshal_list_with(review_output_model, code=200)
     def get(self, place_id):
         """Get all reviews for a specific place"""
-        # Placeholder for logic to return a list of reviews for a place
-        pass
+        try:
+            reviews = facade.review_manager.get_reviews_by_place(place_id)
+            return reviews
+        except PlaceNotFound:
+            api.abort("Place not found", 404)
