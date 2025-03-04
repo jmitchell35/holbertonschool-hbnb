b79edf4f5ca4761b7282c00f2545a2435bd3590d
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-04 21:18:34 +0100

    Amenity_gateway : deletion of unrequired existence check

--- part2/hbnb_layered_facades/app/persistence/gateways/amenity_gateway.py
+++ part2/hbnb_layered_facades/app/persistence/gateways/amenity_gateway.py
@@ -5,9 +5,6 @@ class AmenityGateway(InMemoryRepository):
     def __init__(self):
         super().__init__()
 
-    def amenity_exists(self, amenity):
-        return self.get_by_attribute('name', amenity)
-
     def add(self, obj):
         self._storage[obj.id] = obj
         return obj
