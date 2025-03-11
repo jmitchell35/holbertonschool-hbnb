da5dcc292db0368d7a472bc25f0507a752c72e7a
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-11 15:24:52 +0100

    User endpoints updated with custom decorators @admin_required and @user_matches_or_admin

--- part3/hbnb_layered_facades/app/api/v1/user_endpoints.py
+++ part3/hbnb_layered_facades/app/api/v1/user_endpoints.py
@@ -1,6 +1,8 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt
+from app.api.v1.authentication_utils import (admin_required,
+                                             user_matches_or_admin)
 from app.services.exception import (EmailAlreadyExists, InvalidUserData,
                                     UserNotFound, UnauthorizedAccess)
 
@@ -50,16 +52,10 @@ class UserList(Resource):
             return {'error': 'Invalid input data'}, 400
 
     @api.response(200, 'Users list retrieved successfully')
-    @jwt_required()
+    @admin_required  # decorator performs token check in place of jwt_required()
     def get(self):
         """Retrieve list of users requires admin access token"""
-        try:
-            current_user = get_jwt()
-            if current_user['is_admin'] == True:
-                return facade.user_facade.get_all_users()
-            raise UnauthorizedAccess
-        except UnauthorizedAccess:
-            api.abort(403, error='You need to be admin')
+        return facade.user_facade.get_all_users()
 
 @api.route('/<user_id>')
 # user_id comes from route, not payload / body
@@ -67,49 +63,38 @@ class UserList(Resource):
 class UserResource(Resource):
     @api.response(200, 'User details retrieved successfully')
     @api.response(404, 'User not found')
-    @jwt_required()
+    @user_matches_or_admin  # Same as line 55
     def get(self, user_id):
         """Get user details by ID requires a user matching JWT token"""
         try:
             current_user = get_jwt()
             user = facade.user_facade.get(user_id)
-            if current_user['is_admin'] == True or current_user['sub'] == user_id:
-                return {
-                    "message": f"Hello, user {current_user['sub']}"
-                    }, 200
-            else:
-                raise UnauthorizedAccess
+            return {
+                "message": f"Hello, user {current_user['sub']}"
+                }, 200
         except UserNotFound:
             return {'error': 'User not found'}, 404
-        except UnauthorizedAccess:
-            return {'error': 'Unauthorized access'}, 403
 
     @api.expect(user_model, validate=True)
     @api.response(200, 'User details updated successfully')
     @api.response(404, 'User not found')
     @api.response(400, 'Email already registered')
     @api.response(400, 'Invalid input data')
-    @jwt_required()
+    @user_matches_or_admin  # Same as line 55
     def put(self, user_id):
         """Update one user details requires user matching or admin rights token"""
         try:
-            current_user = get_jwt()
             user = facade.user_facade.get(user_id)
-            if current_user['is_admin'] == True or current_user['sub'] == user_id:
-                uptd_user = facade.user_facade.update_user(user, api.payload)
-                return {
-                    'id': uptd_user.id,
-                    'first_name': uptd_user.first_name,
-                    'last_name': uptd_user.last_name,
-                    'email': uptd_user.email
-                    }, 200
-            else:
-                raise UnauthorizedAccess 
+            uptd_user = facade.user_facade.update_user(user, api.payload)
+            return {
+                'id': uptd_user.id,
+                'first_name': uptd_user.first_name,
+                'last_name': uptd_user.last_name,
+                'email': uptd_user.email
+            }, 200
         except UserNotFound:
             return {'error': 'User not found'}, 404
         except EmailAlreadyExists:
             return {'error': 'Email already registered'}, 400
         except InvalidUserData:
             return {'error': 'Invalid input data'}, 400
-        except UnauthorizedAccess:
-            return {'error': 'Unauthorized access'}, 403
