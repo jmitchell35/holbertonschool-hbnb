ac29632fff10f9461affb1d45c7c72590a5770bc
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-11 15:22:26 +0100

    Moving namespaces registration to the v1 Blueprint

--- part3/hbnb_layered_facades/app/__init__.py
+++ part3/hbnb_layered_facades/app/__init__.py
@@ -1,6 +1,5 @@
-from flask import Flask, jsonify
-from flask_restx import Api
 from config import DevelopmentConfig
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
@@ -11,28 +10,15 @@ bcrypt = Bcrypt()
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        doc='/api/v1/',
-        prefix='/api/v1/'
-    )
-    
-    from app.api.v1.user_endpoints import api as users_ns
-    api.add_namespace(users_ns, path='/users')
-    from app.api.v1.place_endpoints import api as places_ns
-    api.add_namespace(places_ns, path='/places')
-    from app.api.v1.amenity_endpoints import api as amenities_ns
-    api.add_namespace(amenities_ns, path='/amenities')
-    from app.api.v1.review_endpoints import api as review_ns
-    api.add_namespace(review_ns, path='/reviews')
-    from app.api.v1.auth import api as auth_ns
-    api.add_namespace(auth_ns, path='/login')
 
+    # Makes bcrypt hashing available through app object
     bcrypt.init_app(app)
 
+    # initialize jwt auth handling through app object
     jwt.init_app(app)
+
+    # Register v1 Blueprint (which registers api namespaces)
+    from app.api.v1 import api_v1
+    app.register_blueprint(api_v1)
 
     return app
