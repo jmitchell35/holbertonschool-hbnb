cc9a5e6bfa6b65c1da371d9178270c6f6d6d0c60
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-12 13:03:46 +0100

    Adding base admin user, modifying delete place operation

--- part3/hbnb_layered_facades/app/persistence/gateways/user_gateway.py
+++ part3/hbnb_layered_facades/app/persistence/gateways/user_gateway.py
@@ -4,6 +4,9 @@ from app.persistence.gateways.repository import InMemoryRepository
 class UserGateway(InMemoryRepository):
     def __init__(self):
         super().__init__()
+        from app.models.user_model import User
+        user = User('Admin', 'Admin', 'admin@gmail.com', 'password', True)
+        self.add(user)
 
     def add(self, obj):
         self._storage[obj.id] = obj
@@ -20,7 +23,6 @@ class UserGateway(InMemoryRepository):
         (obj.reviews).remove(review_id)
         return obj
 
-    def delete_place(self, obj_id, place_id):
-        obj = self.get(obj_id)
-        (obj.places).remove(place_id)
-        return obj
+    def delete_place(self, owner, place_id):
+        owner.places.remove(place_id)
+        return owner
