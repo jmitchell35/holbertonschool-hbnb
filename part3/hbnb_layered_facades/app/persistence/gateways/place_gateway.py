c13b61df2d1966c8ddbfcbc2a0ee66f85fefa015
jmitchell35 <julian.m.mitchell@gmail.com>
2025-03-15 14:00:48 +0100

    Deleted delete method for clean fresh start

--- part3/hbnb_layered_facades/app/persistence/gateways/place_gateway.py
+++ part3/hbnb_layered_facades/app/persistence/gateways/place_gateway.py
@@ -4,9 +4,3 @@ class PlaceGateway(SQLAlchemyRepository):
     def __init__(self):
         from app.models.place_model import Place
         super().__init__(Place)
-    
-
-    # A revoir
-    def delete_review(self, obj, review_id):
-        (obj.reviews).remove(review_id)
-        return obj
