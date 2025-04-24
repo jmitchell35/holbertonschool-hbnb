d1bac3b80902ea0f20e4b6a62ec0ba2571bfbf7a
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-24 16:53:46 +0200

    Giving more specific CORS settings

--- part3/hbnb_layered_facades/app/__init__.py
+++ part3/hbnb_layered_facades/app/__init__.py
@@ -15,7 +15,9 @@ db = SQLAlchemy()
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app)
+    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:5500", "http://127.0.0.1:5500"], 
+                                     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                                     "allow_headers": ["Content-Type", "Authorization"]}})
 
     # Makes bcrypt hashing available through app object
     bcrypt.init_app(app)
