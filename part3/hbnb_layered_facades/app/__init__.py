81a4b9ec9e978f8fe7f59f8f8457a5b8f7dd2c8c
jmitchell35 <julian.m.mitchell@gmail.com>
2025-04-24 16:49:17 +0200

    Fixing URL typo, import mistake in app __init__

--- part3/hbnb_layered_facades/app/__init__.py
+++ part3/hbnb_layered_facades/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-from flask import CORS
+from flask_cors import CORS
 
 jwt = JWTManager()
 
--- part4/client/js/page-specific/login.js
+++ part4/client/js/page-specific/login.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         console.log('Attempting login with:', { email });
 
-        const response = await fetch('http://localhost:5000//api/v1/auth/login', {
+        const response = await fetch('http://localhost:5000/api/v1/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
