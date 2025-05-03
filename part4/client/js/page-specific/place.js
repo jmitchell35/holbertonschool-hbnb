1d55a48bd53fb83ba0e9e43b6424eb88437b4e96
jmitchell35 <julian.m.mitchell@gmail.com>
2025-05-04 00:12:21 +0200

    Work in progress on building review cards

--- part4/client/js/page-specific/place.js
+++ part4/client/js/page-specific/place.js
@@ -37,7 +37,7 @@ class Place {
 
 function displayPlace(result) {
   const mainElement = mainIsFlex();
-  mainElement.style.justifyContent = 'space-between';
+  mainElement.style.justifyContent = 'space-around';
   mainElement.style.alignItems = 'stretch';
   console.log(result);
   const pageTitle = document.getElementsByTagName('title')[0];
@@ -59,28 +59,86 @@ function displayPlace(result) {
   card.setAttribute('id', `${place.id}`);
   card.style.backgroundColor = '#ffffff';
   card.style.display = 'flex';
+  card.style.justifyContent = 'space-between';
+  card.style.paddingBlock = '40px';
   card.style.borderRadius = '10px';
-  card.style.boxShadow = 'rgba(0, 0, 0, 0.24) 0px 3px 8px';
+  card.style.boxShadow = '0 3px 10px rgb(0 0 0 / 0.2)';
   card.style.flexGrow = '2';
 
   const host = document.createElement('span');
   host.innerHTML = `<b>Host:</b> ${place.owner.first_name} ${place.owner.last_name}`;
   card.appendChild(host);
+  host.style.padding = '8px 0 8px 0';
 
   const price = document.createElement('span');
   price.innerHTML = `<b>Price per night:</b> ${place.price}$`;
   card.appendChild(price);
+  price.style.padding = '8px 0 8px 0';
 
   const description = document.createElement('span');
   description.innerHTML = `<b>Description:</b> ${place.description}`;
   card.appendChild(description);
+  description.style.padding = '8px 0 8px 0';
 
   const amenities = document.createElement('span');
   amenities.innerHTML = `<b>Amenities:</b> ${place.amenities}`;
   card.appendChild(amenities);
-
-
+  amenities.style.padding = '8px 0 8px 0';
 }
+
+function buildReviewCard(parentElement, review) {
+  const card = document.createElement('article');
+
+  card.setAttribute('id', `${review.id}`);
+  card.style.margin = '20px 0 20px 0';
+  card.style.padding = '30px';
+  card.style.backgroundColor = '#ffffff';
+  card.style.borderRadius = '15px';
+  card.style.padding = '15px';
+  card.style.alignItems = 'center';
+  card.style.textAlign = 'center';
+  parentElement.appendChild(card);
+
+  const cardContent = document.createElement('div');
+  card.appendChild(cardContent);
+  cardContent.style.textAlign = 'left';
+
+  const cardTitle = document.createElement('h4');
+  cardTitle.textContent = `${review.user.first_name} ${review.user.last_name}:`;
+  cardContent.appendChild(cardTitle);
+  cardTitle.style.padding = '8px 0 8px 0';
+
+  const cardReview = document.createElement('p');
+  cardReview.textContent = `${review.text}`;
+  cardContent.appendChild(cardReview);
+  cardReview.style.padding = '8px 0 8px 0';
+
+  const cardRating = document.createElement('p');
+  cardRating.textContent = `Rating: ${review.rating}`;
+  cardContent.appendChild(cardRating);
+  cardRating.style.padding = '8px 0 8px 0';
+}
+
+function displayReviews(result) {
+  const mainElement = document.getElementsByTagName('main')[0];
+  const refElement = document.getElementById('reviews');
+  
+  const title = document.createElement('h2');
+  title.textContent = 'Reviews';
+  title.style.textAlign = 'left';
+  title.style.margin = '20px 0 20px 0';
+
+  refElement.parentNode.insertBefore(title, refElement);
+
+  const section = document.getElementById('reviews');
+
+  for (let review of result.data.reviews) {
+    buildReviewCard(section, review);
+  }
+  
+}
+
+//CODE
 // URL = Protocol://domain/path?querystring. Isolates query string
 const urlParams = new URLSearchParams(window.location.search);
 const addReviewSection = document.getElementById('add-review');
@@ -97,6 +155,7 @@ if (token) {
 
 document.addEventListener('DOMContentLoaded', async () => {
   const placeResult = await placePromise;
-  let place = placeResult.data;
+  place = placeResult.data;
   displayPlace(placeResult);
+  displayReviews(placeResult);
 });
